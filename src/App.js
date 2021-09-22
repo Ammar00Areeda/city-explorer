ee3575e8c96a89a9fc69567e8c40be5c83b2951c
Ammar00Areeda <akiareeda@yahoo.com>
2021-09-22 18:21:42 +0300

    front-end

--- src/App.js
+++ src/App.js
@@ -1,61 +1,135 @@
 import React from 'react'
 import axios from 'axios';
+import Main from './main';
+
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      locationResult: {},
-      searchQuery: '',
-      showLocInfo: false
+      weatherDataInfo: []
+
     }
   }
-  getLocFun = async (e) => {
-    e.preventDefault();
-    console.log('inside getLocFun')
-    await this.setState({
-      searchQuery: e.target.city.value
-    })
-    let reqUrl = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`;
 
-    let locResult = await axios.get(reqUrl);
-    
-    
+  getWeatherInfo = async () => {
+    let url = `localhost:3001/getWeater?searchQuery=Amman `;
+    let weatherData = await axios.get(url);
     this.setState({
-      locationResult: locResult.data[0],
-      showLocInfo: true
-    })  
+      weatherDataInfo: weatherData.data
+    })
+    console.log(this.state.weatherDataInfo);
+    console.log(weatherData);
   }
-
-
-
-
+  
 
   render() {
     return (
       <div>
-        <h3>City Explorer app</h3>
-        {/* <button onClick={this.getLocFun}>Get Location</button> */}
-        <form onSubmit={this.getLocFun} >
-          <input type="text" name='city' />
-          <input type="submit" value='get city info' />
-        </form>
-
-        {this.state.showLocInfo &&
-          <>
-            <p>City name: {this.state.searchQuery}</p>
-            <p>latitude: {this.state.locationResult.lat}</p>
-            <p>longitude: {this.state.locationResult.lon} </p>
-
-            <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.locationResult.lat},${this.state.locationResult.lon}&zoom=10`} alt="city" />
-
-          </>
-        }
-
+        <button onClick={this.getWeatherInfo}>get weather</button>
+        <p>l</p>
       </div>
     )
   }
 
+
 }
-export default App
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       locationResult: {},
+//       searchQuery: '',
+//       showLocInfo: false
+//     }
+//   }
+//   getLocFun = async (e) => {
+//     e.preventDefault();
+//     console.log('inside getLocFun')
+//     await this.setState({
+//       searchQuery: e.target.city.value
+//     })
+//     let reqUrl = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`;
+
+//     let locResult = await axios.get(reqUrl);
+
+
+//     this.setState({
+//       locationResult: locResult.data[0],
+//       showLocInfo: true
+//     })  
+//   }
+
+
+
+
+
+//   render() {
+//     return (
+//       <div>
+//         <h3>City Explorer app</h3>
+//         {/* <button onClick={this.getLocFun}>Get Location</button> */}
+//         <form onSubmit={this.getLocFun} >
+//           <input type="text" name='city' />
+//           <input type="submit" value='get city info' />
+//         </form>
+
+//         {this.state.showLocInfo &&
+//           <>
+//             <p>City name: {this.state.searchQuery}</p>
+//             <p>latitude: {this.state.locationResult.lat}</p>
+//             <p>longitude: {this.state.locationResult.lon} </p>
+
+//             <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.locationResult.lat},${this.state.locationResult.lon}&zoom=10`} alt="city" />
+
+//           </>
+//         }
+
+//       </div>
+//     )
+//   }
+
+// }
+// export default App
