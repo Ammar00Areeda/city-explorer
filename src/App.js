1a396eeea22d7146e0f1a24a1d71898a7afbbe67
Ammar00Areeda <akiareeda@yahoo.com>
2021-09-26 15:23:09 +0300

    test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import React from 'react'
 import axios from 'axios';
 
+// https://city-explore-backend.herokuapp.com
+
 
 
 
@@ -10,13 +12,17 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      weatherDataInfo: {}
+      weatherDataInfo: {},
+      setWeatherInfo: false,
+
+
 
     }
   }
 
   getLocFun = async (e) => {
     e.preventDefault();
+    console.log(e.target.city.value);
     console.log('inside getLocFun')
     await this.setState({
       searchQuery: e.target.city.value
@@ -29,31 +35,33 @@ class App extends React.Component {
     this.setState({
       locationResult: locResult.data[0],
       showLocInfo: true
-    })  
+    })
   }
 
   getWeatherInfo = async () => {
-    let url = `https://city-explore-backend.herokuapp.com/getWeater?searchQuery=Amman `;
+    console.log(this.state.searchQuery);
+    let url = `${process.env.REACT_APP_SERVER_LINK}/getWeater?searchQuery=${this.state.searchQuery}`;
     let weatherData = await axios.get(url);
     this.setState({
-      weatherDataInfo : weatherData.data
+      weatherDataInfo: weatherData.data,
+      setWeatherInfo: true,
     })
     console.log(this.state.weatherDataInfo);
     console.log(weatherData);
   }
-  
+
 
   render() {
     return (
       <div>
-         <h3>City Explorer app</h3>
-         {/* <button onClick={this.getLocFun}>Get Location</button> */}
-         <form onSubmit={this.getLocFun} >
-           <input type="text" name='city' />
+        <h3>City Explorer app</h3>
+        {/* <button onClick={this.getLocFun}>Get Location</button> */}
+        <form onSubmit={this.getLocFun} >
+          <input type="text" name='city' />
           <input type="submit" value='get city info' />
-         </form>
+        </form>
 
-         {this.state.showLocInfo &&
+        {this.state.showLocInfo &&
           <>
             <p>City name: {this.state.searchQuery}</p>
             <p>latitude: {this.state.locationResult.lat}</p>
@@ -62,19 +70,31 @@ class App extends React.Component {
             <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.locationResult.lat},${this.state.locationResult.lon}&zoom=10`} alt="city" />
 
           </>
+
         }
-
         <button onClick={this.getWeatherInfo}>get weather</button>
-        <p>weather information in consol</p>
-        
 
-      </div>
-    )
+        {this.state.setWeatherInfo &&
+         
+        
+        <>
+            
+              {this.state.weatherDataInfo.map((iteam,index)=>{
+                return(<div key={index}>
+                  <p>{iteam.data}</p>
+                  <p>{iteam.description}</p>
+                </div>)
+                
+              }) }
+            </>
+        }
+            </div>
+            )
   }
 
 
 }
-export default App;
+            export default App;
 
 
 
@@ -124,7 +144,7 @@ export default App;
 //       showLocInfo: false
 //     }
 //   }
-  
+
 
 
 
